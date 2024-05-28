a336c505b865a384f11750d01f9f3ec43b9350ae
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-05-28 21:50:05 +0100

    adds FeedbackToken.sol

---
+++ src/FeedBackToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract FeedbackToken is ERC20, Ownable, ERC20Permit {
+    // Contract that decrypts signed message and extracts `to` and `amount`
+    address manager;
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Caller is not manager");
+        _;
+    }
+
+    constructor(address _manager) ERC20("FeedbackToken", "FBT") Ownable(msg.sender) ERC20Permit("FeedbackToken") {
+        manager = _manager;
+    }
+
+    function mint(address to, uint256 amount) public onlyManager {
+        _mint(to, amount);
+    }
+}
