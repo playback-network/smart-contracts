0fb6225f7f23e10a5aabb75f72d11a243a76ec41
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-05-29 21:31:21 +0100

    adds setSignedMinter to PlayBackToken

--- src/PlayBackToken.sol
+++ src/PlayBackToken.sol
@@ -5,17 +5,25 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract PlaybackToken is ERC20, Ownable, ERC20Permit {
-    // Contract that decrypts signed message and extracts `to` and `amount`
-    address manager;
+contract PlayBackToken is ERC20, Ownable, ERC20Permit {
+    // Contract that decrypts signed message and extracts `recipient` and `amount`
+    address signedMinter;
 
     modifier onlyManager() {
-        require(msg.sender == manager, "Caller is not manager");
+        require(msg.sender == signedMinter, "Caller is not manager");
         _;
     }
 
-    constructor(address _manager) ERC20("PlaybackToken", "BACK") Ownable(msg.sender) ERC20Permit("FeedbackToken") {
-        manager = _manager;
+    constructor(address _signedMinter)
+        ERC20("PlayBackToken", "BACK")
+        Ownable(msg.sender)
+        ERC20Permit("PlayBackToken")
+    {
+        signedMinter = _signedMinter;
+    }
+
+    function setSignedMinter(address addr) external onlyOwner {
+        signedMinter = addr;
     }
 
     function mint(address to, uint256 amount) public onlyManager {
