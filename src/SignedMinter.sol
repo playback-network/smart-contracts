da69e69e4f1d210964b3dfb8fd6a176381d30090
FullStackPho <20524533+ph0ph0@users.noreply.github.com>
2024-05-29 17:57:55 +0100

    WIP: fixing sig rec

--- src/SignedMinter.sol
+++ src/SignedMinter.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.23;
+// pragma solidity ^0.8.23;
 
 import "../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import "./PlayBackToken.sol";
@@ -14,6 +14,7 @@ contract SignedMinter is Ownable {
 
     constructor(address _tokenAddress, address _payloadSigner) Ownable(msg.sender) {
         token = PlaybackToken(_tokenAddress);
+        payloadSigner = _payloadSigner;
     }
 
     function mint(bytes memory signature, uint256 tokenAmount, address recipient) public {
@@ -26,43 +27,13 @@ contract SignedMinter is Ownable {
     }
 
     function _verify(bytes memory signature, uint256 tokenAmount, address recipient) public view returns (bool) {
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenAmount, recipient));
-        bytes32 signedMessageHash = MessageHashUtils.toEthSignedMessageHash(messageHash);
-        // (address recoveredAddress, ECDSA.RecoverError errorReason, bytes32 errorMessage) =
-        //     ECDSA.tryRecover(signedMessageHash, signature);
+        bytes32 message = keccak256(abi.encodePacked(tokenAmount, recipient));
+        address recoveredAddress = recoverStringFromRaw(message, signature);
 
-        // if (errorReason == ECDSA.RecoverError.NoError) {
-        //     return recoveredAddress == payloadSigner;
-        // } else {
-
-        // }
-        // return false;
-
-        recoverSignerFromSignature(signedMessageHash, signature);
+        return recoveredAddress == payloadSigner;
     }
 
-    function recoverSignerFromSignature(bytes32 message, bytes memory sig) internal pure returns (address) {
-        require(sig.length == 65);
-
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        return ecrecover(message, v, r, s);
-    }
-
-    // --------------------------------------------------------------------
-
-    function recoverStringFromRaw(string memory message, bytes memory sig) public pure returns (address) {
+    function recoverStringFromRaw(bytes32 message, bytes memory sig) public pure returns (address) {
         require(sig.length == 65, "invalid signature length");
 
         bytes32 r;
@@ -80,25 +51,8 @@ contract SignedMinter is Ownable {
             v += 27;
         }
 
-        bytes32 messageHash =
-            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n", uintToStr(bytes(message).length), message));
+        bytes32 signedMessageHash = MessageHashUtils.toEthSignedMessageHash(message);
 
-        return ecrecover(messageHash, v, r, s);
-    }
-
-    function uintToStr(uint256 v) internal pure returns (string memory str) {
-        uint256 maxlength = 100;
-        bytes memory reversed = new bytes(maxlength);
-        uint256 i = 0;
-        while (v != 0) {
-            uint256 remainder = v % 10;
-            v = v / 10;
-            reversed[i++] = bytes1(uint8(48 + remainder));
-        }
-        bytes memory s = new bytes(i);
-        for (uint256 j = 0; j < i; j++) {
-            s[j] = reversed[i - j - 1];
-        }
-        str = string(s);
+        return ecrecover(signedMessageHash, v, r, s);
     }
 }
